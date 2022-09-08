1c4499d17decbfe1dd94a26fe575b8729c90c934
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-08 18:19:49 +0300

    виконала 2 таск

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery'); // !
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup); // !
+
+// ! 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+}
+
+galleryContainerEl.addEventListener('click', openModal);
+
+// ! 2. Реалізація відключення переходу на нову сторінку при кліку на лінк
+function openModal(event) {
+  event.preventDefault();
+}
+
+// ! 3.  Ініціалізація бібліотеки
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
